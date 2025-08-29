140dc94d70253bafa42d8356bd766346907c07a0
Janne Nowak <j.nowak@maerz-network.de>
2025-08-29 12:14:24 +0200

    Merged Vscode init file to normal init

--- init.lua
+++ init.lua
@@ -1,27 +1,32 @@
 vim.g.mapleader = " "
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.g.vscode then
+  -- bootstrap lazy and all plugins
+  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  if not vim.uv.fs_stat(lazypath) then
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  end
+
+  vim.opt.rtp:prepend(lazypath)
+
+  local lazy_config = require "configs.lazy"
+
+  -- load plugins
+  require("lazy").setup({
+    { import = "plugins" },
+  }, lazy_config)
 end
-
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_config = require "configs.lazy"
-
--- load plugins
-require("lazy").setup({
-  { import = "plugins" },
-}, lazy_config)
-
-vim.cmd.colorscheme "catppuccin"
 
 require "options"
 require "autocmds"
 
 vim.schedule(function()
-  require "mappings"
+  if vim.g.vscode then
+    require "mappings-vscode"
+  else
+    vim.cmd.colorscheme "catppuccin"
+    require "mappings"
+  end
 end)
