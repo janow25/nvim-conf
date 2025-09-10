1db475715fbd0c9727ed9d9721805dac75c670c9
Janne Nowak <janne.now@icloud.com>
2025-09-10 13:56:51 +0300

    Adopted Bufferline to work with snacks

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,33 +1,36 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	lazy = false,
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		vim.opt.termguicolors = true
-		require("bufferline").setup({
-			options = {
-				mode = "buffers",
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "File Tree",
-						separator = false,
-						text_align = "left",
-					},
-					{
-						filetype = "TelescopePrompt",
-						text = "Telescope",
-						separator = false,
-						text_align = "left",
-					},
-				},
-				diagnostics = "nvim_lsp",
-				separator_style = { "", "" },
-				modified_icon = "●",
-				show_close_icon = false,
-				show_buffer_close_icons = true,
-			},
-		})
-	end,
+  "akinsho/bufferline.nvim",
+  version = "*",
+  lazy = false,
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup({
+      options = {
+        mode = "buffers",
+        offsets = {
+          {
+            filetype = "snacks_layout_box",
+          },
+          {
+            filetype = "neo-tree",
+            text = "File Tree",
+            separator = false,
+            text_align = "left",
+          },
+          {
+            filetype = "TelescopePrompt",
+            text = "Telescope",
+            separator = false,
+            text_align = "left",
+          },
+        },
+        diagnostics = "nvim_lsp",
+        separator_style = { "", "" },
+        modified_icon = "●",
+        show_close_icon = false,
+        show_buffer_close_icons = true,
+      },
+    })
+  end,
 }
