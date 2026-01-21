42c189c3b81ac929d323b54542e014bf673864f7
Janne Nowak <janne.now@icloud.com>
2026-01-21 18:11:05 +0100

    improved lspconfig structure

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,80 +1,58 @@
-local lspconfig = require "lspconfig"
-local util = require "lspconfig.util"
+local lspconfig = vim.lsp.config
+local util = require("lspconfig.util")
 
--- List of servers to setup with default options
+-- Configure LSP servers (only setup + configuration lives in this file)
 local servers = {
-  "marksman",
-  "html",
-  "cssls",
-  "pyright",
-  "jsonls",
-  "ts_ls",
-  "gopls",
-  "dockerls",
-  "helm_ls",
-  "yamlls",
-  "bashls",
-  "rust_analyzer",
-}
+  marksman = {},
+  html = {},
+  cssls = {},
+  pyright = {},
+  jsonls = {},
+  ts_ls = {},
+  gopls = {},
+  dockerls = {},
+  helm_ls = {},
+  yamlls = {},
 
-for _, server in ipairs(servers) do
-  lspconfig[server].setup {}
-end
-
-require('lspconfig').lua_ls.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
-}
-
--- Rust Analyzer with root_dir and settings
-lspconfig.rust_analyzer.setup {
-  root_dir = util.root_pattern "Cargo.toml",
-  settings = {
-    ["rust-analyzer"] = {
-      diagnostics = { enable = false },
-    },
-  },
-}
-
--- Bash with filetypes and settings
-lspconfig.bashls.setup {
-  filetypes = { "sh" },
-  settings = {
-    bashIde = {
-      globPattern = "*@(.sh|.inc|.bash|.command)",
-    },
-  },
-}
-
--- Custom Intersystems LSP registration
-local configs = require "lspconfig.configs"
-if not configs.intersystems_ls then
-  configs.intersystems_ls = {
-    default_config = {
-      cmd = { "/home/jno/linux-x86_64/intersystems-ls" },
-      filetypes = { "objectscript", "objectscript_class" },
-      root_dir = function(fname)
-        return util.root_pattern "*.cls"(fname) or util.find_git_ancestor(fname)
-      end,
-      settings = {
-        objectscript = {
-          conn = {
-            active = true,
-            host = "mymhs.maerz-network/mhs",
-            ns = "MHSLIB",
-            superPort = 443,
-            username = "_system",
-            password = "sys",
-          },
-        },
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = { globals = { "vim" } },
       },
     },
-  }
+  },
+
+  rust_analyzer = {
+    root_dir = util.root_pattern("Cargo.toml"),
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = { enable = false },
+      },
+    },
+  },
+
+  bashls = {
+    filetypes = { "sh" },
+    settings = {
+      bashIde = {
+        globPattern = "*@(.sh|.inc|.bash|.command)",
+      },
+    },
+  },
+}
+
+local function configure_and_enable(server, opts)
+  if type(vim.lsp.config) == "function" then
+    vim.lsp.config(server, opts or {})
+  else
+    vim.lsp.config[server] = vim.tbl_deep_extend("force", vim.lsp.config[server] or {}, opts or {})
+  end
+
+  if type(vim.lsp.enable) == "function" then
+    vim.lsp.enable(server)
+  end
 end
 
-lspconfig.intersystems_ls.setup {}
+for server, opts in pairs(servers) do
+  configure_and_enable(server, opts)
+end
